56586989f37816656e2920db26312398bf10632d
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2023-04-06 17:13:22 -0300

    Add repository

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.8.2',
+        'version' => '1.8.2.0',
+        'reference' => 'ec6e38806ee62e593174fac6797da68714d319a4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.8.2',
+            'version' => '1.8.2.0',
+            'reference' => 'ec6e38806ee62e593174fac6797da68714d319a4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'masnathan/odtphp' => array(
+            'pretty_version' => 'dev-teclib',
+            'version' => 'dev-teclib',
+            'reference' => '6c2f07dd82993565ab6536a30d2d6c04ab699086',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../masnathan/odtphp',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
